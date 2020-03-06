d72a2c9e5ef4b8166d97c5ce7e13c93f78e5864e
Julia Foresti <juliajforesti@gmail.com>
2020-03-06 17:44:03 -0300

    parcial

--- js/index.js
+++ js/index.js
@@ -1,7 +1,81 @@
 window.onload = () => {
+  let canvas = document.getElementById('canvas');
+  let context = canvas.getContext("2d");
+  
   document.getElementById('start-button').onclick = () => {
     startGame();
   };
+  
+  
+  function startGame() {
+    window.requestAnimationFrame(carUpdate);
 
-  function startGame() {}
+    // let road = new Image();
+    // road.src = './images/road.png';
+    // road.onload = () => {
+    //   context.drawImage(road, 0, 0);
+    // }
+
+    // carUpdate();
+
+    // interval = setInterval(car.carUpdate, 20);
+
+  }
+  class Component {
+  constructor(x, y, width, height){
+      this.x = x;
+      this.y = y;
+      this.width = width;
+      this.height = height;
+    }
+
+    moveLeft(){
+      this.x -= 20;
+      console.log('move left')
+    }
+  
+    moveRight(){
+      this.x += 20;
+      console.log('move right')
+    }
+  }
+
+  let car = new Component(115, 330, 50, 80);
+
+  function carUpdate(){
+
+
+    // "CLEAR" (BG)
+    let road = new Image();
+    road.src = './images/road.png';
+    road.onload = () => {
+      context.drawImage(road, 0, 0);
+    }
+
+
+    // PRINT O CARRO
+    let carImg = new Image();
+    carImg.src = './images/car.png';
+    carImg.onload = () => {
+      context.drawImage(carImg, car.x, car.y, car.width, car.height);
+    }
+
+    window.requestAnimationFrame(carUpdate);
+
+  }
+   
+  
+  document.onkeydown = function(e) {
+    switch (e.keyCode) {
+      case 37: // left arrow
+        car.moveLeft();
+        break;
+      case 39: // right arrow
+        car.moveRight();
+        break;
+    }
+  }
+
 };
+
+
